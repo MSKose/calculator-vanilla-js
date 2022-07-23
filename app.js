16375ec8052c18793ed582368ed1e57a96349493
MSKose <saitmustafa1998@gmail.com>
2022-07-23 14:59:55 +0200

    added operator functionality

--- app.js
+++ app.js
@@ -2,21 +2,44 @@ const btnContent = document.querySelector('.content');
 const preDisplay = document.querySelector('.previous-display');
 const curDisplay = document.querySelector('.current-display');
 
-console.log(curDisplay);
-
-
 let currentNumber = '';
 let previousNumber = '';
+let operation = '';
 
 btnContent.addEventListener('click', (e) => {
     if (e.target.classList.contains("num")){
-        updateNumber(e.target.textContent)
-        updateDisplay()
+        updateNumber(e.target.textContent);
+        updateDisplay();
     }
 
     if (e.target.classList.contains("operator")){
-        updateNumber(e.target.textContent)
-        updateDisplay()
+        addOperator(e.target.textContent);
+        updateDisplay();
+    }
+
+    if (e.target.classList.contains("equal")){
+        calculate();
+        updateDisplay();
+    }
+    
+    if (e.target.classList.contains("ac")){
+        currentNumber = '';
+        previousNumber = '';
+        operation = '';
+        updateDisplay();
+    }
+
+    if (e.target.classList.contains("abs")){
+        if(!currentNumber) return;
+        currentNumber *= -1;
+        updateDisplay();
+    }
+
+    if (e.target.classList.contains('percent')) {
+        if (!currentNumber) return;
+        currentNumber = currentNumber / 100;
+        updateDisplay();
+        equalOrPercentPressed = true;
     }
 });
 
@@ -25,7 +48,13 @@ function updateNumber(num) {
     if (num === "." && currentNumber.includes('.')) return;
 
     // preventing leading 0
-    if (!currentNumber && num === '0') return;
+    if (currentNumber === '0' && num === '0') return;
+
+    // only allowing 0. lead
+    if (currentNumber === '0' && num !== '.') {
+        currentNumber = num;
+        return;
+    }
 
     // limiting maximum input length to 7
     // if (currentNumber.length > 6) return;
@@ -36,4 +65,46 @@ function updateNumber(num) {
 
 function updateDisplay() {
     curDisplay.textContent = currentNumber;
+    preDisplay.textContent = `${previousNumber} ${operation}`
+}
+
+function addOperator(op) {
+    // head to calculate function only if previousNumber exists:
+    if (previousNumber){
+        calculate();
+    }
+    
+    operation = op;
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+
+function calculate () {
+    let calculation = 0;
+
+    // converting strings to numbers
+    const pre = Number(previousNumber);
+    const curr = Number(currentNumber);
+
+    // console.log(operation);
+    switch (operation) {
+        case '+':
+          calculation = pre + curr;
+          break;
+        case '-':
+          calculation = pre - curr;
+          break;
+        case 'x':
+          calculation = pre * curr;
+          break;
+        case '÷':
+          calculation = pre / curr;
+          break;
+        default:
+          return;
+    }
+    currentNumber = calculation;
+
+    previousNumber = '';
+    operation = '';
 }
